1a3e8ca03262e39bf0082cd3af9dbb33697ee98f
Tega-Ojcode1 <ojevwetega3@gmail.com>
2023-09-17 15:22:04 +0100

    solved some conditional problems

---
+++ index.js
@@ -0,0 +1,83 @@
+// let errorCode ='MATHS ERROR '
+
+// // q1
+// let sum1 = 33 
+// let sum2 = 2
+// let sum3 = (sum1) + (sum2)
+// console.log(sum3)
+    
+
+// q2
+    // let differ1 = 3 ;
+    // let differ2 = 5;
+    // let differ3 = (differ1) - (differ2)
+
+
+// // q3
+    //     let multi1 = 2 
+    //  let multi2 = 3
+        
+// if (multi == multi2) {
+//     console.log(`THE MULTIPLICATION EQUALS TO :${multi2}`)
+// }
+// else {
+//     console.log(`${errorCode}`)
+// }
+
+// // q4
+// let devi = 5 /5
+// let devi2 = devi 
+//     if (devi == devi2) {
+//        console.log(`THE PRODUCT OF THE DIVISION IS : ${devi2}`) 
+//     }
+//     else {
+//         console.log(`${errorCode}`)
+//     }
+
+// // q5
+// let pers = 3 % 2
+// let pers2 = pers
+// if (pers === pers2) {
+//     console.log(`THE REMAINDER IS : ${pers2}`)
+// }
+// else {
+//     console.log(`${errorCode}`)
+// }
+    
+// // q6
+let area = '5' * 5
+let area2 = area
+if (area == area2) {
+    console.log(`THE AREA OF THE RECTANGLE IS : ${area2}`)
+}
+else {
+    console.log(`${errorCode}`)
+}
+
+// // q7
+// let year = 1517
+// if (year % 4 == 0)
+// {
+
+//     console.log("IT'S A LEAP YEAR")
+// }
+// else {
+//     console.log("IT'S NOT A LEAP YEAR")
+// }
+
+// q8
+// let days = 8
+// let weeks = days / 7
+// let remainderDays = days % 7 
+// if (days /7 >=1)
+// {
+//  console.log(`THAT IS ${weeks} week :)`)   
+// }
+// else if (remainderDays <= days)
+// {
+//     console.log(`${weeks}WEEKS AND ${remainderDays} DAYS`)   
+// }
+// else
+// {
+// console.log('NOT UP TO A WEEK')    
+// }
